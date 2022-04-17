39c23d343f1a34dd95ac035d60680e7f5ff23134
Filipp.filippov <filipp.filippov@sephora.com>
2022-04-17 19:19:09 +0300

    add terraform block to providers

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-central-1"
   shared_credentials_files = ["/root/.aws/credentials"]
