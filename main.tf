65ea86b102f38425e2e8374f8c475b668ab686f2
Filipp.filippov <filipp.filippov@sephora.com>
2022-04-17 18:25:05 +0300

    init for org, 2 OU + 2 accts

--- main.tf
+++ main.tf
@@ -26,30 +26,23 @@ resource "aws_organizations_organization" "tft-test" {
 resource "aws_organizations_organizational_unit" "dev-ou" {
   name      = "dev"
   parent_id = aws_organizations_organization.tft-test.roots[0].id
-
-  accounts  {
-    name  = aws_organizations_account.development.name
-    id    = aws_organizations_account.development.id
-  }
 }
 
 resource "aws_organizations_organizational_unit" "prod-ou" {
   name      = "prod"
   parent_id = aws_organizations_organization.tft-test.roots[0].id
 
-  accounts  {
-    name = aws_organizations_account.production.name
-    id    = aws_organizations_account.development.id
-  }
 }
 
 resource "aws_organizations_account" "development" {
+  parent_id = aws_organizations_organizational_unit.dev-ou.id
   name      = local.account_name["development"]
   email     = local.account_owner_email["development"]
   role_name = "Admin"
 }
 
 resource "aws_organizations_account" "production" {
+  parent_id = aws_organizations_organizational_unit.prod-ou.id
   name      = local.account_name["production"]
   email     = local.account_owner_email["production"]
   role_name = "Admin"
