1f87a3c46e539271bd513667c70e2293eb5bd22b
Filipp.filippov <filipp.filippov@sephora.com>
2022-04-17 19:14:21 +0300

    update mamin tf with new syntax

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -1,13 +1,12 @@
 resource "aws_s3_bucket" "tfstate" {
   bucket = var.bucket_name
-  acl    = "private"
+  acl = "private"
+}
 
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
