2c508c1fdf4898bab185a06087864ab5e7e3b450
Filipp.filippov <filipp.filippov@sephora.com>
2022-04-19 15:05:19 +0300

    refactor

--- main.tf
+++ main.tf
@@ -13,49 +13,5 @@ provider "aws" {
   profile = "default"
 }
 
-provider "aws" {
-  assume_role {
-    role_arn = "arn:aws:iam::${aws_organizations_account.development.id}:role/Admin"
-  }
 
-  alias  = "development"
-  region = "eu-central-1"
-}
-
-provider "aws" {
-  assume_role {
-    role_arn = "arn:aws:iam::${aws_organizations_account.production.id}:role/Admin"
-  }
-
-  alias  = "production"
-  region = "eu-central-1"
-}
-
-resource "aws_organizations_organization" "tft-test" {
-}
-
-resource "aws_organizations_organizational_unit" "dev-ou" {
-  name      = "dev"
-  parent_id = aws_organizations_organization.tft-test.roots[0].id
-}
-
-resource "aws_organizations_organizational_unit" "prod-ou" {
-  name      = "prod"
-  parent_id = aws_organizations_organization.tft-test.roots[0].id
-
-}
-
-resource "aws_organizations_account" "development" {
-  parent_id = aws_organizations_organizational_unit.dev-ou.id
-  name      = local.account_name["development"]
-  email     = local.account_owner_email["development"]
-  role_name = "Admin"
-}
-
-resource "aws_organizations_account" "production" {
-  parent_id = aws_organizations_organizational_unit.prod-ou.id
-  name      = local.account_name["production"]
-  email     = local.account_owner_email["production"]
-  role_name = "Admin"
-}
 
---
+++ modules/organization/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  assume_role {
+    role_arn = "arn:aws:iam::${aws_organizations_account.development.id}:role/Admin"
+  }
+
+  alias  = "development"
+  region = "eu-central-1"
+}
+
+provider "aws" {
+  assume_role {
+    role_arn = "arn:aws:iam::${aws_organizations_account.production.id}:role/Admin"
+  }
+
+  alias  = "production"
+  region = "eu-central-1"
+}
+
+resource "aws_organizations_organization" "tft-test" {
+}
+
+resource "aws_organizations_organizational_unit" "this_ou" {
+  for_each  = toset(var.OUs)
+  name      = each.key
+  parent_id = aws_organizations_organization.tft-test.roots[0].id
+}
+
+resource "aws_organizations_account" "development" {
+  parent_id = aws_organizations_organizational_unit.this_ou.id
+  name      = local.account_name["development"]
+  email     = local.account_owner_email["development"]
+  role_name = "Admin"
+}
+
+resource "aws_organizations_account" "production" {
+  parent_id = aws_organizations_organizational_unit.this_ou.id
+  name      = local.account_name["production"]
+  email     = local.account_owner_email["production"]
+  role_name = "Admin"
+}
