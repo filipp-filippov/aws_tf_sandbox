9cfef6a2a030073e1b21cc743447d256e5009926
Filipp.filippov <filipp.filippov@sephora.com>
2022-04-30 14:51:37 +0300

    add TF IAM role

--- modules/mgmt/iam/main.tf
+++ modules/mgmt/iam/main.tf
@@ -2,13 +2,22 @@ data "template_file" "tf_permissions" {
   template = file("${path.module}/terraform_role.json")
 }
 
+data "aws_caller_identity" "current" {}
+
+data "template_file" "tf_assume_role" {
+  template = file("${path.module}/assume_role.tpl")
+  vars  = {
+    account_id  = data.aws_caller_identity.current.account_id
+  }
+}
+
 resource "aws_iam_policy" "mgmt-tf" {
   name = var.tf_policy_name
   policy = data.template_file.tf_permissions.rendered
 }
 
 resource "aws_iam_role" "mgmt-tf-role" {
-  assume_role_policy = aws_iam_policy.mgmt-tf
+  assume_role_policy = data.template_file.tf_assume_role
   tags = {
     env = var.iam_role_env
   }
