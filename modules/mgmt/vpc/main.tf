a70bf9d0c99e61d4026991382dd9e3fa5f007fdd
Filipp.filippov <filipp.filippov@sephora.com>
2022-05-01 04:15:17 +0300

    update az value

--- modules/mgmt/vpc/main.tf
+++ modules/mgmt/vpc/main.tf
@@ -8,7 +8,7 @@ resource "aws_default_vpc" "mgmt-default" {
 resource "aws_subnet" "mgmt-svc" {
   vpc_id     = aws_default_vpc.mgmt-default.id
   cidr_block = "172.16.24.0/21"
-  availability_zone = "eu-central-1"
+  availability_zone = "eu-central-1a"
   tags = {
     Name = "svc"
   }
@@ -17,7 +17,7 @@ resource "aws_subnet" "mgmt-svc" {
 resource "aws_subnet" "mgmt-compute" {
   vpc_id     = aws_default_vpc.mgmt-default.id
   cidr_block = "172.16.32.0/21"
-  availability_zone = "eu-central-1"
+  availability_zone = "eu-central-1b"
   tags = {
     Name = "compute"
   }
@@ -26,7 +26,7 @@ resource "aws_subnet" "mgmt-compute" {
 resource "aws_subnet" "mgmt-db" {
   vpc_id     = aws_default_vpc.mgmt-default.id
   cidr_block = "172.16.40.0/21"
-  availability_zone = "eu-central-1"
+  availability_zone = "eu-central-1c"
   tags = {
     Name = "db"
   }
