55841721a56496d2c4f17190bd80c4f3ca4eac64
Filipp.filippov <filipp.filippov@sephora.com>
2022-05-02 07:28:48 +0300

    refactor

--- modules/mgmt/main.tf
+++ modules/mgmt/main.tf
@@ -15,15 +15,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-central-1"
-  shared_credentials_files = ["/root/.aws/credentials"]
-  profile = "default"
-}
-
-provider "aws" {
   assume_role {
     role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TerraformMGMTRole"
   }
+  shared_credentials_files = ["/root/.aws/credentials"]
   alias  = "mgmt"
   region = "eu-central-1"
   profile = "default"
